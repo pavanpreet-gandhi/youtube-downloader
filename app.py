87a6079d25a187752912dd1894fd8e0f9dcaed0a
Pavanpreet Singh Gandhi <pavanpreet.gandhi@gmail.com>
2024-06-02 21:13:54 +0100

    Working streamlit app

--- app.py
+++ app.py
@@ -1,14 +1,45 @@
+import logging
 import streamlit as st
 from src import download_youtube_video
 
+
 def app():
-    st.title("Download a YouTube video")
-    youtube_url = st.text_input("Enter the YouTube URL")
-    download_button = st.button("Download")
 
-    if download_button:
-        download_youtube_video(url=youtube_url, output_dir='audio')
-        st.write("Download complete!")
+    st.title('Download a YouTube video')
+    youtube_url = st.text_input('Enter the YouTube URL')
+    fetch_button = st.button('Fetch YouTube Video')
 
-if __name__ == "__main__":
+    # Create a placeholders for the fetching message and download button
+    fetching_message_placeholder = st.empty()
+    download_button_placeholder = st.empty()
+
+    if fetch_button and youtube_url:
+
+        # Display a fetching message
+        fetching_message_placeholder.text('Fetching YouTube video...')
+
+        # Fetch the YouTube video and convert it to MP3
+        try:
+            download_youtube_video(url=youtube_url, output_dir='audio')
+        except Exception as e:
+            logging.error(f'An error occurred: {e}')
+            fetching_message_placeholder.text('YouTube URL is invalid or the video is not available. Please try again.')
+            return
+
+        # Read the file data
+        with open('audio/audio.mp3', 'rb') as f:
+            file_data = f.read()
+
+        # Replace the placeholder with the download button
+        download_button_placeholder.download_button(
+            label = 'Download MP3',
+            data = file_data,
+            file_name = 'audio.mp3',
+            mime = 'audio/mpeg',
+        )
+
+
+if __name__ == '__main__':
+
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
     app()
